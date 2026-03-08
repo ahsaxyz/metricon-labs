330df71a2632ad317cc1a896f4f04f03c5e7022b
ahsa <solahsa17@gmail.com>
2026-03-08 00:16:00 -0500

    Update SwapTab.tsx

--- src/components/tabs/SwapTab.tsx
+++ src/components/tabs/SwapTab.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { ArrowDownUp, Eye, Loader2, Wallet, AlertCircle, MapPin } from "lucide-react";
-import TokenSelector, { TOKENS, type Token } from "../TokenSelector";
+import TokenSelector, { TOKENS, type Token } from "../ui/TokenSelector";
 import { useWalletConnection } from "@/hooks/useWalletConnection";
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { useSwap, SWAP_TOKENS } from "@/hooks/useSwap";
