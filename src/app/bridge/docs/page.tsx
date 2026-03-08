eab6707e68e1bb5d791cc1eca52ce7ce8d290378
ahsa <solahsa17@gmail.com>
2026-03-08 00:31:02 -0500

    Update page.tsx

--- src/app/bridge/docs/page.tsx
+++ src/app/bridge/docs/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/ui/Navbar";
 import {
   ChevronRight,
   Search,
